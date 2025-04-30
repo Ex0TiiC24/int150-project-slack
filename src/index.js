c79e40845d94a0088f2203db62b28bdba458fa9d
Poon-Poonyawat <91997823+PywtP49@users.noreply.github.com>
2025-04-30 14:24:11 +0700

    start js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const counters = document.querySelectorAll(".stat+h1");
+const container = document.querySelector(".comp-stat-container");
+
+let activated = false;
+
+
+const updateCount = () =>{
+    const target = parseInt(counter.dataset.count);
+
+    if ()
+}
+
+window.addEventListener("scoll",()=>{
+    if (scrollY > container - container.scrollHeight -200 && activated == false){
+        counters.forEach(counters => {
+            counters.innerText = 0;
+            let count = 0
+
+        })
+
+    }
+
+})
