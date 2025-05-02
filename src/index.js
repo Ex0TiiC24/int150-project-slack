9be278b54ce5b824ed00a2cd54519a1d10537d2b
Taiyo Yamamoto <120293524+Ex0TiiC24@users.noreply.github.com>
2025-05-02 15:23:46 +0700

    finished stats

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-const counters = document.querySelectorAll(".stat+h1");
 const container = document.querySelector(".comp-stat-container");
-
+const statContainers = document.querySelectorAll('.comp-stat');
 let activated = false;
 
 
 const updateCount = () =>{
-    const target = parseInt(counter.dataset.count);
-
-    if ()
+    console.log("activated")
+    
+    statContainers.forEach(statContainer =>{
+        const statDiv = statContainer.querySelector('.stat'); 
+        const h1 = statDiv.querySelector('h1');
+        
+        const target = parseInt(h1.dataset.count);
+        let current = 0;
+        const increment = target/50;
+        const interval = setInterval(()=>{
+            current += increment;
+            if (current >= target){
+                current = target;
+                clearInterval(interval)
+            }
+            h1.textContent = Math.floor(current);
+        },50)
+        
+    })
+    
 }
+window.addEventListener("scroll",()=>{
+    const rect = container.getBoundingClientRect();
 
-window.addEventListener("scoll",()=>{
-    if (scrollY > container - container.scrollHeight -200 && activated == false){
-        counters.forEach(counters => {
-            counters.innerText = 0;
-            let count = 0
-
-        })
-
+    if((rect.top >= 0 && rect.top <= window.innerHeight) ||  (rect.bottom >= 0 && rect.bottom <= window.innerHeight)){
+        if(!activated){
+            activated = true
+            updateCount()
+        }
+        
+    }
+    else if (rect.bottom < 0 || rect.top > window.innerHeight) {
+        if (activated) {
+            activated = false; 
+            console.log('unactive');
+        }
     }
 
 })
